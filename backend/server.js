ca696e2640dc364c3234b04eaa68dee5aa5b7f03
AnshulAnand <anandanshul001@gmail.com>
2023-01-05 14:33:10 +0530

    Added user authentication

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
+const colors = require('colors')
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+// Connect to database
+connectDB()
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.send('Hello')
@@ -9,6 +18,8 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
